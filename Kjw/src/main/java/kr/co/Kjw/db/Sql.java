f0f21785a2c67bc1692e092eaea1f6ea9d20b92c
kim_3838 <kim_3838@naver.com>
2021-07-08 12:24:55 +0900

    2021/07/08 12:24 register DB연결 (ajax적용X)

--- Kjw/src/main/java/kr/co/Kjw/db/Sql.java
+++ Kjw/src/main/java/kr/co/Kjw/db/Sql.java
@@ -3,4 +3,17 @@ package kr.co.Kjw.db;
 public class Sql {
 
 	public static final String SELECT_TERMS = "SELECT * FROM `BOARD_TERMS`";
+	public static final String INSERT_MEMBER = "INSERT INTO `BOARD_MEMBER` SET "
+											+ "`uid`=?,"
+											+ "`pass`=?,"
+											+ "`name`=?,"
+											+ "`nick`=?,"
+											+ "`email`=?,"
+											+ "`hp`=?,"
+											+ "`zip`=?,"
+											+ "`addr1`=?,"
+											+ "`addr2`=?,"
+											+ "`regip`=?,"
+											+ "`rdate`=NOW()";
+	
 }
